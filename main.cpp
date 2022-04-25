ef20f9a290f269508ae5152f564ed3c53177d781
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-04-25 19:35:17 +0930

    general

--- main.cpp
+++ main.cpp
@@ -8,13 +8,18 @@ using namespace std;
 
 int main()
 {
+    int s = 20;
+    int x;
     vector<int> a;
-    a.push_back(1);
-    a.push_back(2);
-    a.push_back(-3);
-    a.push_back(4);
-    a.push_back(0);
-    MapSquare mapT; 
+    cout << "Enter Vector: ";
+    for (int i = 0; i < s; i++)
+    {
+        cin>>x;
+        a.push_back(x);
+    }
+    
+    
+    MapTriple mapT; 
     vector<int> b;
     b = mapT.map(a);
 
